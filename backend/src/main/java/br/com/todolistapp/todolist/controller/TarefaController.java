fcdd7f0863605e4c90de4867e3ff3a26dceedbcb
unknown <victorcamposbortolotto@gmail.com>
2021-11-01 20:33:14 -0300

    Criada classe TarefaController, para mapeamento e execução dos métodos desenvolvidos na classe  TarefaService

---
+++ backend/src/main/java/br/com/todolistapp/todolist/controller/TarefaController.java
@@ -0,0 +1,45 @@
+package br.com.todolistapp.todolist.controller;
+
+import br.com.todolistapp.todolist.model.Tarefa;
+import br.com.todolistapp.todolist.service.TarefaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefa")
+public class TarefaController {
+
+    private TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+    @GetMapping
+    public List findAll(){
+        return tarefaService.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable long id){
+        return tarefaService.findById(id);
+    }
+
+    @PostMapping
+    public Tarefa create(@RequestBody Tarefa tarefa){
+        return tarefaService.create(tarefa);
+    }
+
+    @PutMapping(value = {"/{id}"})
+    public ResponseEntity update(@PathVariable("id") long id, @RequestBody Tarefa tarefa){
+        return tarefaService.update(id, tarefa);
+    }
+
+    @DeleteMapping(path = {"/{id}"})
+    public ResponseEntity delete(@PathVariable long id){
+        return tarefaService.delete(id);
+    }
+
+}
